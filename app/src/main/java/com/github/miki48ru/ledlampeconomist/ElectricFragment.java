ca8c3dfc2a213cc33510086330d4c1a7537cae40
Miki48RU <miki48ru@gmail.com>
2016-05-11 08:51:56 +0300

    создаем переменные для организации логики

--- app/src/main/java/com/github/miki48ru/ledlampeconomist/ElectricFragment.java
+++ app/src/main/java/com/github/miki48ru/ledlampeconomist/ElectricFragment.java
@@ -11,13 +11,43 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ElectricFragment extends Fragment {
     public ElectricFragment() {
         // Required empty public constructor
     }
+
+    private float priceRateOne;
+    private float priceRateTwo;
+
+    private static List<Integer> listSpinnerHours = new ArrayList<>();// список для выбора работы лампочки в часах
+    private static List<Integer> listSpinnerRates2Hours = new ArrayList<>();// список для выбора работы лампочки в часах по тарифу 2
+    private static List<Integer> listSpinnerPercent = new ArrayList<>(); // список для выбора процента удорожания
+
+    TextView textView;
+    Spinner spinnerHour;
+    Spinner spinnerRatesTwoHour;
+    Spinner spinnerPercent;
+    private int resultTimeYears;
+    private int resultTimeYearsTwoRate;
+    private int selected3;
+    private int selected3TwoRate;
+    private int percent;
+    private float summPrice;
+    private float summPriceTwoRate;
+
+
+
+
+
+
 
 
     @Override
