dab587ccfe6203dc2d9b616dadb1d5f77c16af1c
Miki48RU <miki48ru@gmail.com>
2016-05-13 16:54:20 +0300

    добавлены фрагменты и анимация, которая пока не работает

--- app/src/main/java/com/github/miki48ru/ledlampeconomist/ElectricFragment.java
+++ app/src/main/java/com/github/miki48ru/ledlampeconomist/ElectricFragment.java
@@ -5,15 +5,21 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.transition.TransitionManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.Switch;
-import android.widget.TextView;
+import android.support.v7.widget.SwitchCompat;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,22 +37,37 @@ public class ElectricFragment extends Fragment {
     private static List<Integer> listSpinnerRates2Hours = new ArrayList<>();// список для выбора работы лампочки в часах по тарифу 2
     private static List<Integer> listSpinnerPercent = new ArrayList<>(); // список для выбора процента удорожания
 
-    TextView textView;
     Spinner spinnerHour;
     Spinner spinnerRatesTwoHour;
     Spinner spinnerPercent;
+
+    EditText mPrice;
+    EditText mPrice2;
+
     private int resultTimeYears;
     private int resultTimeYearsTwoRate;
-    private int selected3;
-    private int selected3TwoRate;
+    private int selectedHour;
+    private int selectedHourTwoRate;
     private int percent;
-    private float summPrice;
-    private float summPriceTwoRate;
+    private float summPrice = 0;
+    private float summPriceTwoRate = 0;
+
+    private boolean checked;
+    final String LOG_TAG = "myLogs";
 
 
-
-
-
+    // статически заполняем списки выборов так как никогда не меняются значения
+    static {
+        for (int i = 1; i <= 24; i++) {
+            listSpinnerHours.add(i);
+        }
+        for (int i = 1; i <= 24; i++) {
+            listSpinnerRates2Hours.add(i);
+        }
+        for (int i = 0; i <=50; i++){
+           listSpinnerPercent.add(i);
+        }
+    }
 
 
 
@@ -61,12 +82,27 @@ public class ElectricFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        spinnerHour = (Spinner)getActivity().findViewById(R.id.spinner_hours);
+        spinnerRatesTwoHour = (Spinner)getActivity().findViewById(R.id.spinner_hours_2);
+        spinnerPercent = (Spinner)getActivity().findViewById(R.id.spinner_percent);
+
+        mPrice = (EditText)getActivity().findViewById(R.id.et_price);
+        mPrice2 = (EditText)getActivity().findViewById(R.id.et_price_2);
+
+        String price = mPrice.getText().toString();
+
+        summPrice = Float.parseFloat(String.valueOf(mPrice));
+        Log.d(LOG_TAG, "summ: " + summPrice);
+        summPriceTwoRate = Float.parseFloat(mPrice2.getText().toString());
+
         Button btNext = (Button)getActivity().findViewById(R.id.btNextElectric);
         btNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ViewPager viewPager = (ViewPager) getActivity().findViewById(R.id.viewpager);
                 viewPager.setCurrentItem(1);
+
+
             }
         });
 
@@ -81,12 +117,13 @@ public class ElectricFragment extends Fragment {
         params.height = 0;
         tax_layout.setLayoutParams(params);
 
-        Switch swTax = (Switch)getActivity().findViewById(R.id.sw_double_tax);
+        SwitchCompat swTax = (SwitchCompat) getActivity().findViewById(R.id.sw_double_tax);
         swTax.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Switch sw = (Switch)v;
+                SwitchCompat sw = (SwitchCompat) v;
+                checked = ((SwitchCompat) v).isChecked();
                 if (sw.isChecked()) {
                     tax_layout.startAnimation(animationFadeIn);
                     TransitionManager.beginDelayedTransition(sceneRoot);
@@ -109,5 +146,103 @@ public class ElectricFragment extends Fragment {
                 }
             }
         });
+
+        adapterSpinnerHours();
+        adapterSpinnerHours2Rate();
+        adapterSpinnerPercent();
+        setClicklistenerToSpinner();
+        setClicklistenerTwoRatesSpinner();
+        setClicklistenerPercentSpinner();
+
+
     }
+
+
+    public void adapterSpinnerHours() {
+        SpinnerAdapter spinnerAdapter = new ArrayAdapter<Integer>(getContext(),
+                android.R.layout.simple_spinner_item, listSpinnerHours);
+        ((ArrayAdapter<Integer>) spinnerAdapter).setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerHour.setAdapter(spinnerAdapter);
+        spinnerHour.setSelection(1);
+    }
+
+    public void adapterSpinnerHours2Rate() {
+        SpinnerAdapter spinnerAdapter = new ArrayAdapter<Integer>(getContext(),
+                android.R.layout.simple_spinner_item, listSpinnerRates2Hours);
+        ((ArrayAdapter<Integer>) spinnerAdapter).setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerRatesTwoHour.setAdapter(spinnerAdapter);
+        spinnerRatesTwoHour.setSelection(3);
+    }
+
+    public void adapterSpinnerPercent() {
+        SpinnerAdapter spinnerAdapter = new ArrayAdapter<Integer>(getContext(),
+                android.R.layout.simple_spinner_item, listSpinnerPercent);
+        ((ArrayAdapter<Integer>) spinnerAdapter).setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerPercent.setAdapter(spinnerAdapter);
+        spinnerPercent.setSelection(15);
+    }
+
+    public void setClicklistenerToSpinner() {
+        spinnerHour.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+
+                selectedHour = (int) spinnerHour.getSelectedItem();
+                try {
+
+                    resultTimeYears = selectedHour * 365;
+
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    public void setClicklistenerTwoRatesSpinner() {
+        spinnerRatesTwoHour.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+
+                selectedHourTwoRate = (int) spinnerRatesTwoHour.getSelectedItem();
+                try {
+
+                    resultTimeYearsTwoRate = selectedHourTwoRate * 365;
+
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    public void setClicklistenerPercentSpinner() {
+        spinnerPercent.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+
+                percent = (int) spinnerPercent.getSelectedItem();
+                Log.d(LOG_TAG, "percent: " + percent);// проценты из раскрывающегося списка по позиции
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
 }
